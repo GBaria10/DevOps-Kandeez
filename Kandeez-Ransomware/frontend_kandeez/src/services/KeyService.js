5d32bc756759ab564fa869ffdc3ec7c1531cbc61
Gaourang Baria <Gaourang.Baria22@st.niituniversity.in>
2025-04-30 13:54:31 +0530

    Update KeyService.js

--- Kandeez-Ransomware/frontend_kandeez/src/services/KeyService.js
+++ Kandeez-Ransomware/frontend_kandeez/src/services/KeyService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://172.19.18.232:5000/api"; // backend is the docker-compose service name
+const API_URL = "http://192.168.47.134:5000/api"; // backend is the docker-compose service name
 
 
 export const fetchAllKeys = async () => {
