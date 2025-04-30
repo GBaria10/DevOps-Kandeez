a787e76132c674320928da1853de2a8f8b2516fe
Gaourang Baria <Gaourang.Baria22@st.niituniversity.in>
2025-04-30 11:22:09 +0530

    devops testing

--- Kandeez-Ransomware/frontend_kandeez/src/App.jsx
+++ Kandeez-Ransomware/frontend_kandeez/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <nav className="navbar navbar-dark custom-navbar">
         <div className="container d-flex justify-content-center">
           <span className="navbar-brand mb-0 h1 custom-title">
-            Kandeez Ransomware
+            Kandeez Ransomwareeeeee
           </span>
         </div>
       </nav>
