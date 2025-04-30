e54409a245b960458f9da1bb98335ea194b35390
mohitkhandal22 <khandalmohit9588@gmail.com>
2025-04-30 16:41:09 +0530

    change

--- Kandeez-Ransomware/frontend_kandeez/src/App.jsx
+++ Kandeez-Ransomware/frontend_kandeez/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <nav className="navbar navbar-dark custom-navbar">
         <div className="container d-flex justify-content-center">
           <span className="navbar-brand mb-0 h1 custom-title">
-            Kandeez Ransomwareeeeee
+            Kandeez Ransomware
           </span>
         </div>
       </nav>
