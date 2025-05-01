f42f74ba843d6bd955fe4c46512293ca6928d17b
Gaourang Baria <Gaourang.Baria22@st.niituniversity.in>
2025-05-01 11:29:44 +0530

    devops testing

--- Kandeez-Ransomware/frontend_kandeez/src/services/KeyService.js
+++ Kandeez-Ransomware/frontend_kandeez/src/services/KeyService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = "http://172.19.21.163:5000/api"; // backend is the docker-compose service name
+const API_URL = "http://192.168.137.1:5000/api"; // backend is the docker-compose service name
 
 
 export const fetchAllKeys = async () => {
